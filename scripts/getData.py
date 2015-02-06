0ee4c3ac2eb8f0f7db4b361963f79f4faa5dcc1a
Hawken Rives <hawkrives@gmail.com>
2015-02-06 00:16:12 -0600

    Always turn diff values into arrays

--- scripts/getData.py
+++ scripts/getData.py
@@ -183,10 +183,10 @@ def dict_diff(first, second):
 	sd2 = set(second)
 	# Keys missing in the second dict
 	for key in sd1.difference(sd2):
-		diff[key] = KEYNOTFOUNDIN2
+		diff[key] = [KEYNOTFOUNDIN2]
 	# Keys missing in the first dict
 	for key in sd2.difference(sd1):
-		diff[key] = KEYNOTFOUNDIN1
+		diff[key] = [KEYNOTFOUNDIN1]
 	# Check for differences
 	for key in sd1.intersection(sd2):
 		if first[key] != second[key]:
