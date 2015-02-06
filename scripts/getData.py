6732a9e1325d133212ec517436bf21e1d860b349
Hawken Rives <hawkrives@gmail.com>
2015-02-06 01:07:18 -0600

    Return None if a value didn't exist before

--- scripts/getData.py
+++ scripts/getData.py
@@ -197,7 +197,7 @@ def get_old_dict_values(old, new):
 	# Returns the "old" value for two dicts.
 	diff = dict_diff(old, new)
 
-	return {key: diff[key][0] for key in diff}
+	return {key: diff[key][0] if diff[key][0] != KEYNOTFOUNDIN1 else None for key in diff}
 
 
 class Course:
