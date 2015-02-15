84564b3df7e6823a4e6d9e04044afa0a6659b75a
Hawken Rives <hawkrives@gmail.com>
2015-02-14 23:42:35 -0600

    Re-write getData

--- scripts/getData.py
+++ scripts/getData.py
@@ -1,101 +1,89 @@
 #!/usr/bin/env python3
 
 from concurrent.futures import ProcessPoolExecutor
-from collections import OrderedDict
 from argparse import ArgumentParser
-from datetime import datetime
-from bs4 import BeautifulSoup
-import xmltodict
 import functools
-import itertools
-import requests
-import hashlib
-import time
-import json
-import csv
-import os
-import re
 
-from data_processing.lib.maintain_lists_of_entries import maintain_lists_of_entries
-from data_processing.lib.calculate_terms import calculate_terms
-from data_processing.lib.json_folder_map import json_folder_map
-from data_processing.lib.pretty import pretty
-from data_processing.Year import Year
+from lib.fetch_course_details import fetch_course_details
+from lib.calculate_terms import calculate_terms
+from lib.process_courses import process_courses
+from lib.fetch_term_data import load_term
+from lib.flattened import flatten
+from lib.log import log
 
-from data_processing.lib.paths import handmade_path
 
-quiet = False
-dry_run = False
+def get_years_and_terms(terms_or_years):
+	years, terms = [], []
+	for item in terms_or_years:
+		str_item = str(item)
+		if len(str_item) is 4:
+			years.append(item)
+		elif len(str_item) is 5:
+			terms.append(item)
 
-departments = None
-with open(handmade_path + 'to_department_abbreviations.json') as depts:
-	departments = json.loads(depts.read())
+	return years, terms
 
-course_types = None
-with open(handmade_path + 'course_types.json') as types:
-	course_types = json.loads(types.read())
 
-def main():
-	global quiet
-	global dry_run
-
+def get_args():
 	argparser = ArgumentParser(description='Fetch term data from the SIS.')
 
-	argparser.add_argument('--terms', '-t',
-		type=int, nargs='*',
-		help='Terms for which to request data from the SIS.')
+	argparser.add_argument('terms_or_years', type=int, nargs='*', help='Terms (or entire years) for which to request data from the SIS.')
+	argparser.add_argument('--workers', '-w', type=int, default=8, help='Specify the number of terms to run simultaneously.')
+	argparser.add_argument('--force-download-terms', action='store_true', help='Force reloading of the specified terms.')
+	argparser.add_argument('--force-download-details', action='store_true', help='Force reloading of any course details from the specified terms.')
+	argparser.add_argument('--dry-run', '-d', action='store_true', help='Only print output; don\'t write files.')
+	argparser.add_argument('--no-revisions', '-n', action='store_false', help='Prevent searching for revisions of courses.')
+	argparser.add_argument('--quiet', '-q', action='store_true', help='Silence logging; mostly used when looking for data.')
 
-	argparser.add_argument('--years', '-y',
-		type=int, nargs='*',
-		help='Entire years for which to request data from the SIS.')
+	return argparser.parse_args()
 
-	argparser.add_argument('--force', '-f',
-		action='store_true',
-		help='Force reloading of the specified course.')
 
-	argparser.add_argument('--dry', '-d',
-		action='store_true',
-		help='Only print output; don\'t write files.')
+def one_term(term, workers, **kwargs):
+	str_term = str(term)
+	pretty_term = str_term[0:4] + ':' + str_term[4]
 
-	argparser.add_argument('--single-thread', '-s',
-		action='store_true',
-		help='Only run with one thread; also returns useful debugging info')
+	log(pretty_term, 'Loading term')
+	raw_term_data = load_term(term,
+		force_download=kwargs['force_download_terms'])
 
-	argparser.add_argument('--quiet', '-q',
-		action='store_true',
-		help='Silence logging; mostly used when looking for data.')
+	if not raw_term_data:
+		return []
 
-	argparser.add_argument('--output-type',
-		type=str, nargs='?', choices=['json', 'csv'],
-		help='Sets the output filetype.')
+	log(pretty_term, 'Extracting courses')
+	courses = raw_term_data['searchresults']['course']
 
-	args = argparser.parse_args()
-	quiet = args.quiet
-	dry_run = args.dry
+	log(pretty_term, 'Loading details')
+	details = fetch_course_details([c['clbid'] for c in courses],
+		dry_run=kwargs['dry_run'], force_download=kwargs['force_download_details'])
 
-	# Create an amalgamation of single terms and entire years as terms
-	terms = calculate_terms(terms=args.terms, years=args.years)
-	terms_grouped_by_years = {}
-	for key, group in itertools.groupby(terms, lambda term: int(str(term)[0:4])):
-		terms_grouped_by_years[key] = list(group)
+	log(pretty_term, 'Processing courses')
+	final_courses = process_courses(courses, details,
+		dry_run=kwargs['dry_run'], find_revisions=kwargs['find_revisions'])
 
-	# Fire up the Terms
-	mapped_year_processor = functools.partial(Year, args=args, terms=terms_grouped_by_years)
-	years = list(map(mapped_year_processor, terms_grouped_by_years))
-	if not quiet: print('Terms:', pretty([year.get_terms() for year in years]))
+	return final_courses
 
-	[year.process() for year in years]
 
-	json_folder_map(folder=term_dest, kind='courses')
+def main():
+	args = get_args()
+	years, terms = get_years_and_terms(args.terms_or_years)
 
-	all_courses = [course for year in years for term in year.termdata for course in term.courses]
-	maintain_lists_of_entries(all_courses)
+	terms = calculate_terms(years=years, terms=terms)
+
+	edit_one_term = functools.partial(one_term, find_revisions=args.no_revisions, **vars(args))
+
+	if args.workers > 1:
+		with ProcessPoolExecutor(max_workers=args.workers) as pool:
+			courses = list(pool.map(edit_one_term, terms))
+	else:
+		courses = list(map(edit_one_term, terms))
+
+	return terms
 
 
 if __name__ == '__main__':
 	main()
 
-# get data
-# process data
-# maintain lists
-# generate final files
+	# json_folder_map(folder=term_dest, kind='courses')
+
+	# all_courses = [course for year in years for term in year.termdata for course in term.courses]
+	# maintain_lists_of_entries(all_courses)
