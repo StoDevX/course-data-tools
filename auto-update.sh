546472abad9c2f31e256261b6110c25e9331a045
Hawken Rives <hawkrives@gmail.com>
2015-04-16 13:53:27 -0500

    [scripts] update auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+git stash
+
 ./scripts/getData.py 20151 20152 20153 --force-download-terms
+
 git status
+git add .
+
+TODAY=`date`
+
+git commit -m "[data-update] $TODAY"
+git push origin master
 
