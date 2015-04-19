7ef5e61055af3ad44b77b0332f4978643af3dd2b
Hawken Rives <hawkrives@gmail.com>
2015-04-19 13:47:33 -0500

    [scripts] log what happens as it happens

--- auto-update.sh
+++ auto-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHANNEL='#gobbldygook-builds"
+CHANNEL='#gobbldygook-builds'
 SLACKBOT_NAME='databot'
 SLACKBOT_ICON=':moyai:'
 SLACK_URL='https://hooks.slack.com/services/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'
@@ -23,7 +23,7 @@ git status
 
 TODAY=`date`
 
-echo 
+echo
 echo "git commit -m \"[data-update] $TODAY\""
 git commit -m "[data-update] $TODAY"
 
@@ -31,7 +31,7 @@ echo
 echo "git push origin master"
 git push origin master
 
-echo 
+echo
 echo "curl POST https://hooks.slack.com"
 
 curl --silent -X POST --data-urlencode "payload={\"channel\": \"$CHANNEL\", \"username\": \"$SLACKBOT_NAME\", \"text\": \"Data collection completed.\", \"icon_emoji\": \"$SLACKBOT_ICON\"}" $SLACK_URL
