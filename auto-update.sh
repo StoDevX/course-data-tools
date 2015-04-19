b0c7a690c72873439e09624751398fc219616e6b
Hawken Rives <hawkrives@gmail.com>
2015-04-19 09:51:30 -0500

    [scripts] log what happens as it happens

--- auto-update.sh
+++ auto-update.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode 'payload={"channel": "#gobbldygook-builds", "username": "databot", "text": "Beginning data collection.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+CHANNEL='#gobbldygook-builds"
+SLACKBOT_NAME='databot'
+SLACKBOT_ICON=':moyai:'
+SLACK_URL='https://hooks.slack.com/services/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'
 
-./scripts/getData.py 20151 20152 20153 --force-download-terms
+echo "curl POST https://hooks.slack.com"
+curl --silent -X POST --data-urlencode "payload={\"channel\": \"$CHANNEL\", \"username\": \"$SLACKBOT_NAME\", \"text\": \"Commencing data collection.\", \"icon_emoji\": \"$SLACKBOT_ICON\"}" $SLACK_URL
 
+echo
+echo "./scripts/getData.py 2015 --force-download-terms"
+
+./scripts/getData.py 2015 --force-download-terms
+
+echo
+echo "git add ."
 git add .
+
+echo
+echo "git status"
 git status
 
 TODAY=`date`
 
+echo 
+echo "git commit -m \"[data-update] $TODAY\""
 git commit -m "[data-update] $TODAY"
+
+echo
+echo "git push origin master"
 git push origin master
 
-curl -X POST --data-urlencode 'payload={"channel": "#gobbldygook-builds", "username": "databot", "text": "Data collection completed.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+echo 
+echo "curl POST https://hooks.slack.com"
+
+curl --silent -X POST --data-urlencode "payload={\"channel\": \"$CHANNEL\", \"username\": \"$SLACKBOT_NAME\", \"text\": \"Data collection completed.\", \"icon_emoji\": \"$SLACKBOT_ICON\"}" $SLACK_URL
