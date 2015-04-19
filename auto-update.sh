a810671ee557c19f8379b3602becaf04d62c8e6b
Hawken Rives <hawkrives@gmail.com>
2015-04-19 00:13:40 -0500

    [scripts] post to slack when updating

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl -X POST --data-urlencode 'payload={"channel": "#gobbldygook-builds", "username": "databot", "text": "Beginning data collection.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+
 ./scripts/getData.py 20151 20152 20153 --force-download-terms
 
 git add .
@@ -10,4 +12,4 @@ TODAY=`date`
 git commit -m "[data-update] $TODAY"
 git push origin master
 
-curl -X POST --data-urlencode 'payload={"channel": "#gobbldygook-builds", "username": "webhookbot", "text": "This is posted to #gobbldygook-builds and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#gobbldygook-builds", "username": "databot", "text": "Data collection completed.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
