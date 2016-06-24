d46e23ee1e720c95b344286ff23cfafb4a984f9e
Hawken Rives <hawkrives@gmail.com>
2016-06-24 18:18:41 -0500

    update paths for new locations

--- lib/paths.py
+++ lib/paths.py
@@ -1,10 +1,13 @@
-details_source = './course-data/details/'
-xml_source = './course-data/raw_xml/'
-course_dest = './course-data/courses/'
-info_path = './build/info.json'
-term_dest = './build/terms/'
-mappings_path = './course-data/data/generated/'
-handmade_path = './course-data/data/handmade/'
+from os.path import join
+
+COURSE_DATA = '../course-data'
+details_source = join(COURSE_DATA, 'details')
+xml_source = join(COURSE_DATA, 'raw_xml')
+course_dest = join(COURSE_DATA, 'courses')
+info_path = join(COURSE_DATA, 'info.json')
+term_dest = join(COURSE_DATA, 'terms')
+mappings_path = join(COURSE_DATA, 'data-lists')
+handmade_path = join(COURSE_DATA, 'data-mappings')
 
 
 def find_details_subdir(clbid):
@@ -13,22 +16,22 @@ def find_details_subdir(clbid):
     n_thousand = int(int(clbid) / 1000)
     thousands_subdir = (n_thousand * 1000)
 
-    return str(thousands_subdir).zfill(5) + '/' + str_clbid
+    return join(str(thousands_subdir).zfill(5), str_clbid)
 
 
 def make_course_path(clbid):
     clbid = str(clbid).zfill(10)
-    return course_dest + find_details_subdir(clbid) + '.json'
+    return join(course_dest, find_details_subdir(clbid) + '.json')
 
 
 def make_html_path(clbid):
     clbid = str(clbid).zfill(10)
-    return details_source + find_details_subdir(clbid) + '.html'
+    return join(details_source, find_details_subdir(clbid) + '.html')
 
 
 def make_xml_term_path(term):
-    return xml_source + str(term) + '.xml'
+    return join(xml_source, str(term) + '.xml')
 
 
 def make_built_term_path(term, kind, folder=term_dest):
-    return folder + str(term) + '.' + kind
+    return join(folder, str(term) + '.' + kind)
