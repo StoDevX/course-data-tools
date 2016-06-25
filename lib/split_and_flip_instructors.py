9f75e8b0933bc827cbdcd4c6cbd00145d0890d12
Hawken Rives <hawkrives@gmail.com>
2016-06-24 20:12:12 -0500

    simplify split_and_flip_instructors

--- lib/split_and_flip_instructors.py
+++ lib/split_and_flip_instructors.py
@@ -5,20 +5,12 @@ def split_and_flip_instructors(course):
     # Take a string like 'Bridges IV, William H.' and split/flip it
     # to 'William H. Bridges IV'. Oh, and do that for each professor.
 
-    if 'instructors' in course:
-        instructors = parse_links_for_text(course['instructors'])
-        flipped_profs = []
+    flipped_profs = []
+    instructors = parse_links_for_text(course.get('instructors', []))
 
-        for prof in instructors:
-            string_to_split = prof.split(',')
-            actual_name = ''
+    for prof in instructors:
+        string_to_split = reversed(prof.split(','))
+        actual_name = ' '.join([name_part.strip() for name_part in string_to_split])
+        flipped_profs.append(actual_name.strip())
 
-            for name_part in reversed(string_to_split):
-                name_part = name_part.strip()
-                actual_name += name_part + ' '
-
-            flipped_profs.append(actual_name.strip())
-
-        return flipped_profs
-
-    return course.get('instructors', [])
+    return flipped_profs
