8655258069a001e8eba450abcbcd4dd25e1ca78f
Hawken Rives <hawkrives@gmail.com>
2016-06-25 01:16:06 -0500

    make the csv saving return a string

--- lib/csvify.py
+++ lib/csvify.py
@@ -1,25 +1,21 @@
-from .log import log
-from .ensure_dir_exists import ensure_dir_exists
 import csv
-import os
+import io
 
 
 def get_all_keys(list_of_objects):
     return sorted(set([key for obj in list_of_objects for key in obj.keys()]))
 
 
-def save_data_as_csv(data, filepath):
-    ensure_dir_exists(filepath)
-    filename = os.path.split(filepath)[1]
+def csvify(data):
     for item in data:
         if 'revisions' in item:
             item.pop('revisions')
         for key in item:
             if type(item[key]) is str:
                 item[key] = item[key].replace('\n', '\\n')
-    with open(filepath, 'w', newline='') as outfile:
+    with io.StringIO() as outfile:
         csv_file = csv.DictWriter(outfile, get_all_keys(data))
         csv_file.writeheader()
         csv_file.writerows(data)
-
-    log('Wrote', filename, 'term data; %d bytes.' % (len(data)))
+        str_contents = outfile.getvalue()
+    return str_contents
