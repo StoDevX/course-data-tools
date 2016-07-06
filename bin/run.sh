206600a126c443f9aa93dd39bdb38e1674a8bcdd
Hawken Rives <hawkrives@gmail.com>
2016-07-06 16:31:08 -0500

    fix up the run.sh script – it now works

--- bin/run.sh
+++ bin/run.sh
@@ -9,23 +9,25 @@ pip3 install --upgrade -r requirements.txt
 
 # prepare the repositories
 cd ../
-git clone https://github.com/stodevx/course-data.git
+if [ ! -d ./course-data ]; then
+	git clone https://github.com/stodevx/course-data.git
+fi
 cd course-data
+git checkout master
 
 # update course data files
 python3 ../course-data-tools/download.py --force-terms 2015 2016
 python3 ../course-data-tools/maintain-datafiles.py
 git add .
-git commit -m "update course data for $DATE"
-# git push origin master
+git commit -m "course data update $(date)" || (echo "No updates found." && exit 0)
+git push origin master
 
 # update bundled information for public consumption
-git branch -d gh-pages
-git checkout --orphan gh-pages
+git checkout gh-pages
+git merge -m "merge course data" master
 python3 ../course-data-tools/bundle.py --format json xml csv
 
 # remove the source files
-git rm -rf courses/ details/ raw_xml/
 git add .
 git commit -m 'course data bundles'
-# git push -f origin gh-pages
+git push -f origin gh-pages
