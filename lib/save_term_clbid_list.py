40834148b7c25f9d01f3457cc2c1dc50395cbd14
Hawken Rives <hawkrives@gmail.com>
2016-07-06 16:30:15 -0500

    make sure the directory exists before saving the clbid lists

--- lib/save_term_clbid_list.py
+++ lib/save_term_clbid_list.py
@@ -1,10 +1,12 @@
 """Save the list of clbids for a term into a file"""
 
 import json
+from .ensure_dir_exists import ensure_dir_exists
 from .paths import make_clbid_map_path, term_clbid_mapping_path
 
 
 def save_term_clbid_list(term, clbids):
+    ensure_dir_exists(term_clbid_mapping_path)
     with open(make_clbid_map_path(term), 'w', encoding='utf-8') as outfile:
         json_data = json.dumps(clbids, outfile, indent='\t', separators=(',', ': '))
         outfile.write(json_data + '\n')
