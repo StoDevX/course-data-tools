3a3bd7d12c6dffe5cc30e855c04b43429f3dd983
Hawken Rives <hawkrives@gmail.com>
2016-08-06 18:53:07 +0900

    drastically simplify check_for_course_revisions

--- lib/check_for_course_revisions.py
+++ lib/check_for_course_revisions.py
@@ -11,45 +11,50 @@ from .paths import make_course_path
 
 def load_previous(course_path):
     try:
-        prior_data = load_data_from_file(course_path)
+        with open(course_path, 'r', encoding='utf-8') as infile:
+            return json.load(infile)
     except FileNotFoundError:
-        prior_data = None
-
-    if prior_data:
-        try:
-            prior = json.loads(prior_data)
-        except Exception:
-            log("Error loading JSON from", prior_data)
-    else:
-        prior = None
-
-    revisions = []
-
-    if prior and ('revisions' in prior):
-        revisions = prior['revisions']
-        del prior['revisions']
-
-    return (prior, revisions or [])
-
-
-def check_for_revisions(course, ignore_revisions):
-    prior, revisions = load_previous(make_course_path(course['clbid']))
-
-    if not prior:
         return None
+    except json.JSONDecodeError:
+        log("Error decoding old jsonified course %s", course_path)
 
-    diff = get_old_dict_values(prior, course)
+
+def sort_diff(diff, ignore_revisions):
     ordered_diff = OrderedDict()
     for key in sorted(diff.keys()):
         if key not in ignore_revisions:
             ordered_diff[key] = diff[key]
+    return ordered_diff
 
-    if ordered_diff:
-        now = get_localzone().localize(datetime.now())
-        ordered_diff['_updated'] = now.isoformat()
-        revisions.append(ordered_diff)
 
-    if revisions and ('revisions' not in course or revisions != course.get('revisions')):
-        return revisions
+def check_for_revisions(course, ignore_revisions):
+    prior = load_previous(make_course_path(course['clbid']))
 
-    return None
+    if not prior:
+        return None
+
+    if 'revisions' in prior:
+        revisions = prior['revisions']
+        del prior['revisions']
+    else:
+        return None
+
+    if prior == course:
+        return course.get('revisions', None)
+
+    diff = get_old_dict_values(prior, course)
+    if not diff:
+        return None
+
+    ordered_diff = sort_diff(diff, ignore_revisions=ignore_revisions)
+
+    # TODO: Change these to run in UTC, rather than local time.
+    # Also it's slow. Very slow.
+    # Of the 0.894361s this function takes total on 20151,
+    # the next line accounts for 0.055712s of that.
+    # For only two calls.
+    now = get_localzone().localize(datetime.now())
+    ordered_diff['_updated'] = now.isoformat()
+    revisions.append(ordered_diff)
+
+    return revisions
