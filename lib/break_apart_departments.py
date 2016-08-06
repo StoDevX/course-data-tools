3c6aedf841ea885dc2c91914ac33f55cad4f6993
Hawken Rives <hawkrives@gmail.com>
2016-08-06 19:31:53 +0900

    expand a ternary to multiple lines

--- lib/break_apart_departments.py
+++ lib/break_apart_departments.py
@@ -5,6 +5,8 @@ def break_apart_departments(depts):
     # Split apart the departments, because 'AR/AS' is actually
     # ['ART', 'ASIAN'] departments.
     return [
-        departments[dept.lower()] if dept.lower() in departments.keys() else dept
+        departments[dept.lower()]
+            if dept.lower() in departments
+            else dept
         for dept in depts.split('/')
     ]
