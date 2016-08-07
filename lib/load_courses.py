780504fc82a6c9813c4296b4a130801b27d43fd3
Hawken Rives <hawkrives@gmail.com>
2016-08-07 13:17:58 +0900

    turn load_some_courses into a generator

--- lib/load_courses.py
+++ lib/load_courses.py
@@ -17,11 +17,9 @@ def load_all_courses():
             yield load_course(file.path)
 
 
-def load_course_by_clbid(clbid):
-    return load_course(make_course_path(clbid))
-
-
 def load_some_courses(term):
+    """Load courses from a given term mapping package"""
     with open(make_clbid_map_path(term), 'r', encoding='utf-8') as infile:
         clbids = json.load(infile)
-    return [load_course_by_clbid(clbid) for clbid in clbids]
+    for clbid in clbids:
+        yield load_course(make_course_path(clbid))
