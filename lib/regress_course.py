4242cbd41a35043af4ca2c6bd9ee61464537ba5c
Hawken Rives <hawkrives@gmail.com>
2016-09-28 14:57:41 +0900

    [bundle] check for keys before regressing a course

--- lib/regress_course.py
+++ lib/regress_course.py
@@ -1,9 +1,13 @@
 def regress_course(course):
-    course['depts'] = course['departments']
-    del course['departments']
-    course['desc'] = course['description']
-    del course['description']
-    course['num'] = course['number']
-    del course['number']
-    course['pf'] = course['pn']
-    del course['pn']
+    if 'departments' in course:
+        course['depts'] = course['departments']
+        del course['departments']
+    if 'description' in course:
+        course['desc'] = course['description']
+        del course['description']
+    if 'number' in course:
+        course['num'] = course['number']
+        del course['number']
+    if 'pn' in course:
+        course['pf'] = course['pn']
+        del course['pn']
