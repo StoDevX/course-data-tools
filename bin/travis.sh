5ffd562bc8e9c3fcc876ed7577a3cd76df7b31de
Hawken Rives <hawkrives@fastmail.fm>
2018-08-20 22:58:20 -0500

    try fixing push-to-branch again

--- bin/travis.sh
+++ bin/travis.sh
@@ -19,12 +19,20 @@ for y in $(seq 1994 $(date +%Y)); do
 done | xargs -t -n1 -P1 -- python3 ../download.py --force-terms -w 1
 python3 ../maintain-datafiles.py
 
+if [[ $TRAVIS_BRANCH != "master" ]]; then
+	git checkout -b "$TRAVIS_BRANCH"
+fi
+
 git add .
 git commit -m "course data update $(date)" || (echo "No updates found." && exit 0)
-git push -u "https://$GITHUB_OAUTH@github.com/stodevx/course-data.git" $TRAVIS_BRANCH
+git push "https://$GITHUB_OAUTH@github.com/stodevx/course-data.git" "$TRAVIS_BRANCH"
 
 # prepare the gh-pages branch
-git checkout -B gh-pages master --no-track
+PAGES_BRANCH=gh-pages
+if [[ $TRAVIS_BRANCH != "master" ]]; then
+	PAGES_BRANCH="${TRAVIS_BRANCH}-pages"
+fi
+git checkout -B "$PAGES_BRANCH" "$TRAVIS_BRANCH" --no-track
 
 # update bundled information for public consumption
 python3 ../bundle.py --out-dir ../course-data --format json --format xml --format csv
@@ -34,9 +42,10 @@ python3 ../bundle.py --legacy --out-dir ../course-data/legacy --format json
 git rm -rf --quiet details/ raw_xml/
 
 # and … push
-git add --all ./
-git commit -m "course data bundles" --quiet
 if [[ $TRAVIS_BRANCH == "master" ]]; then
+	git add --all ./
+	git commit -m "course data bundles" --quiet
+
     git push -f "https://$GITHUB_OAUTH@github.com/stodevx/course-data.git" gh-pages
     curl https://nosnch.in/9243a27544
 fi
