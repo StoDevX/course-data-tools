f056485481d9242daa0515fd380d432c2411ae21
Drew Volz <volz.drew@gmail.com>
2023-06-01 22:21:41 -0700

    rename environment variables

--- bin/travis.sh
+++ bin/travis.sh
@@ -19,24 +19,24 @@ for y in $(seq 1994 $(date +%Y)); do
 done | xargs -t -n1 -P1 -- python3 ../download.py --force-terms -w 1
 python3 ../maintain-datafiles.py
 
-if [[ $TRAVIS_BRANCH != "master" ]]; then
-	git checkout --quiet -b "$TRAVIS_BRANCH"
+if [[ $GITHUB_BRANCH != "master" ]]; then
+	git checkout --quiet -b "$GITHUB_BRANCH"
 fi
 
 git add .
 git commit --quiet -m "course data update $(date)" || (echo "No updates found." && exit 0)
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ $GITHUB_BRANCH == "master" ]]; then
 	git push "https://$GITHUB_OAUTH@github.com/StoDevX/course-data.git" master
 else
-	git push --force "https://$GITHUB_OAUTH@github.com/StoDevX/course-data.git" "$TRAVIS_BRANCH"
+	git push --force "https://$GITHUB_OAUTH@github.com/StoDevX/course-data.git" "$GITHUB_BRANCH"
 fi
 
 # prepare the gh-pages branch
 PAGES_BRANCH=gh-pages
-if [[ $TRAVIS_BRANCH != "master" ]]; then
-	PAGES_BRANCH="${TRAVIS_BRANCH}-pages"
+if [[ $GITHUB_BRANCH != "master" ]]; then
+	PAGES_BRANCH="${GITHUB_BRANCH}-pages"
 fi
-git checkout --quiet -B "$PAGES_BRANCH" "$TRAVIS_BRANCH" --no-track
+git checkout --quiet -B "$PAGES_BRANCH" "$GITHUB_BRANCH" --no-track
 
 # update bundled information for public consumption
 # python3 ../bundle.py --out-dir ../course-data --format json --format xml --format csv
@@ -47,7 +47,7 @@ python3 ../bundle.py --legacy --out-dir ../course-data/legacy --format json
 git rm -rf --quiet details/ raw_xml/
 
 # and … push
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ $GITHUB_BRANCH == "master" ]]; then
 	git add --all ./
 	git commit --quiet -m "course data bundles" --quiet
 
