3cf0cd114eed1f3b0a5cd778f7e08baf525f9fc6
Drew Volz <volz.drew@gmail.com>
2023-06-02 23:49:07 -0700

    add github ci

--- bin/run.sh
+++ bin/run.sh
@@ -12,7 +12,7 @@ cd course-data
 git checkout master
 git pull origin master
 
-git config user.name "Heroku Databot"
+git config user.name "Github Databot"
 git config user.email "hawkrives+sto-course-databot@gmail.com"
 
 # update course data files
@@ -30,7 +30,8 @@ fi
 git checkout -B gh-pages master --no-track
 
 # update bundled information for public consumption
-python3 ../bundle.py --out-dir ../course-data --format json --format xml --format csv
+# python3 ../bundle.py --out-dir ../course-data --format json --format xml --format csv
+python3 ../bundle.py --out-dir ../course-data --format json --format xml
 python3 ../bundle.py --legacy --out-dir ../course-data/legacy --format json
 
 # remove the source files (quietly)
@@ -40,5 +41,3 @@ git rm -rf --quiet courses/ details/ raw_xml/
 git add --all ./
 git commit -m "course data bundles" --quiet
 git push -f "https://$GITHUB_OAUTH@github.com/stodevx/course-data.git" gh-pages
-
-curl https://nosnch.in/9243a27544
