11b6fde5c16cc1f36d0c77488096b9d59d4c1df5
Drew Volz <volz.drew@gmail.com>
2023-07-13 23:14:16 -0700

    formatting

--- lib/database.py
+++ lib/database.py
@@ -7,8 +7,7 @@ def insert_course(db, course):
     offerings = course["offerings"] if len(course.get("offerings", [])) else []
     pass_nopass = 1 if "Pass or No Pass (P/N) only" in description else 0
 
-    # Insert into the 'section' table
-    section_row = {
+    db["section"].insert({
         "clbid": int(course["clbid"]),
         "crsid": int(course["crsid"]),
         "credits": course["credits"],
@@ -18,43 +17,31 @@ def insert_course(db, course):
         "term": str(course["term"]),
         "title": course.get("title", ""),
         "year": int(course["year"]),
-    }
+        }, pk="id"
+    ).m2m("description", {
+        "content": description
+        }, pk="id"
+    ).m2m("instructors", {
+        "name": instructors,
+        "fsnum": "",
+        }, pk="id"
+    ).m2m("enrollment", {
+        "status": course["status"],
+        "max": course["max"],
+        "total": course["enrolled"],
+        "firstyear": course.get("firstyear"),
+        "sophomore": course.get("sophomore"),
+        "junior": course.get("junior"),
+        "senior": course.get("senior"),
+        }, pk="id"
+    ).m2m("countstowards", {
+        "gereq": gereqs,
+        "corereq": "",
+        "major": "",
+        "concentration": "",
+        }, pk="id"
+    )
 
-    db["section"].insert(section_row, pk="id").m2m(
-        "course", {
-            "crsid": int(course["crsid"]),
-            "department_1": course["department"],
-            "department_2": "",
-            "level": course["level"],
-            "name": course["name"],
-            "number": course["number"],
-            "repeat": "",
-            "type": course["type"],
-        }, pk="id").m2m(
-        "description", {
-            "content": description,
-        }, pk="id").m2m(
-        "instructors", {
-            "name": instructors,
-            "fsnum": "",
-        }, pk="id").m2m(
-        "enrollment", {
-            "status": course["status"],
-            "max": course["max"],
-            "total": course["enrolled"],
-            "firstyear": course.get("firstyear"),
-            "sophomore": course.get("sophomore"),
-            "junior": course.get("junior"),
-            "senior": course.get("senior"),
-        }, pk="id").m2m(
-        "countstowards", {
-            "gereq": gereqs,
-            "corereq": "",
-            "major": "",
-            "concentration": "",
-        }, pk="id")
-
-    # Insert into the 'meeting' table
     for offering in offerings:
         meeting_row = {
             "clbid": int(course["clbid"]),
